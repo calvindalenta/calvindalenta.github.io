c8a0023eadc19349b4192a16a3be3f5208241540
Calvin Dalenta <calvindalentark@gmail.com>
2019-10-19 00:58:40 +0700

    Fix dragndrop

---
+++ precache-manifest.33e6d7ea0be1cefbfc35f1247543102f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd0356efe0905f78d82a4f9bfb8ba27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebc70cc636b90f3e2a97",
+    "url": "/static/css/main.0fc708f5.chunk.css"
+  },
+  {
+    "revision": "79115e9502a459d7c48d",
+    "url": "/static/js/2.5b79ad61.chunk.js"
+  },
+  {
+    "revision": "ebc70cc636b90f3e2a97",
+    "url": "/static/js/main.191b4c77.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
